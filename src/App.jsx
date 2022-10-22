7c10fbe309587c2c994e272de9f208b1c1573a45
sebastian castiblanco <scastiblanco2003@gmail.com>
2022-10-22 15:49:58 -0500

    configuring dependencies and file structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>hola</div>
+    </div>
+  );
+}
+
+export default App;
